8df1fc1f50ffb24f04e55358b6beaf4f6a8e0058
Garrett Potts <gpotts@radiantblue.com>
2017-09-07 16:42:53 -0400

    Added echo for logging

--- docker/startup/run.sh
+++ docker/startup/run.sh
@@ -14,6 +14,7 @@ fi
 
 if [ -z $MAP_PROXY_HOME ] ; then
    pushd $HOME > /dev/null
+   echo "MAP_PROXY_HOME not defined.  Using ${HOME}/mapproxy"
    mapproxy-util create -t base-config mapproxy
    export MAP_PROXY_HOME=$HOME/mapproxy
    popd > /dev/null  
@@ -22,8 +23,10 @@ fi
 if [ ! -z $MAP_PROXY_HOME ] ; then
    pushd $MAP_PROXY_HOME >/dev/null
    if [ ! -f app.py ] ; then
+      echo "app.py not found, generating a new app.py in $MAP_PROXY_HOME"
       mapproxy-util create -t wsgi-app -f mapproxy.yaml app.py
    fi
+   echo "spawning app.application --thread=$NUMBER_THREADS --processes=$NUMBER_PROCESSES --port=8080"
    spawning app.application --thread=$NUMBER_THREADS --processes=$NUMBER_PROCESSES --port=8080
    popd > /dev/null
 fi
