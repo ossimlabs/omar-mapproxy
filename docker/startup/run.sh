9c28461cb79fd097be6d554003d4145b79bf7da4
Garrett Potts <gpotts@radiantblue.com>
2018-10-08 13:43:03 -0400

    Added support to run as random user

--- docker/startup/run.sh
+++ docker/startup/run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+if [ ! -z "${AWS_ACCESS_KEY}" ] ; then
+  export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
+fi
+
+if [ ! -z "${AWS_SECRET_KEY}" ] ; then
+  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
+fi
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+sed '/^omar/d' /etc/passwd > /tmp/passwd
+echo omar:x:$USER_ID:$GROUP_ID:Default Application User:$HOME:/sbin/nologin >> /tmp/passwd
+
+export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
 
 if [ -z $NUMBER_THREADS ] ; then
   export NUMBER_THREADS=8
